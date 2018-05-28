065173ee100a93d35919d602d7adc50884fae071
wyfbit <wyfncut@163.com>
2018-05-28 22:12:38 +0800

    JavaDoc

--- JavaProgram/src/com/amc/basicknowledge/proxyinstance/Client.java
+++ JavaProgram/src/com/amc/basicknowledge/proxyinstance/Client.java
@@ -1,6 +1,7 @@
 package com.amc.basicknowledge.proxyinstance;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
 
 public class Client {
 	public static void main(String[] args) {
@@ -11,6 +12,13 @@ public class Client {
 		/*
 		 * 通过proxy的newProxyInstance方法来创建我们的代理对象，我们来看看其三个参数
 		 * 第一个参数：handler.getClass().getClassLoader(),我们这里使用handler这个类的classloader对象来加载我们的代理对象
+		 * 第二个参数：realSubject.getClass().getInterfaces(),我们这里为代理对象提供的接口是真实对象所实行的接口，表示我要代理的是该真实对象，这样我就能调用这组接口中的方法
+		 * 第三个参数：handler,我们这里将这个代理对象关联到了上方的InvocationHandler这个对象上
 		 */
+		Subject subject =(Subject)Proxy.newProxyInstance(handler.getClass().getClassLoader(), realSubject.getClass().getInterfaces(), handler);
+		System.out.println(subject.getClass().getName());
+		subject.rent();
+		subject.hello("World!");
+		
 	}
 }
